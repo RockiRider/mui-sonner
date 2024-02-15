049f831a18b3a86cf209c8d1a0b0671896fd47a2
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-02-15 21:48:33 +0000

    dark theme option

--- apps/example-next/pages/_document.tsx
+++ apps/example-next/pages/_document.tsx
@@ -11,16 +11,14 @@ import {
   DocumentHeadTagsProps,
   documentGetInitialProps,
 } from "@mui/material-nextjs/v14-pagesRouter";
-import theme, { roboto } from "../src/theme";
 
 export default function MyDocument(
   props: DocumentProps & DocumentHeadTagsProps
 ) {
   return (
-    <Html lang="en" className={roboto.className}>
+    <Html lang="en">
       <Head>
         {/* PWA primary color */}
-        <meta name="theme-color" content={theme.palette.primary.main} />
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="emotion-insertion-point" content="" />
         <DocumentHeadTags {...props} />
