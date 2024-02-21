d97bc93cda737b76eb2a915d73e394a6b9bba6fb
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-02-21 22:48:54 +0000

    setup

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,10 +1,8 @@
 import Container from "@mui/material/Container";
-import { Button, PaletteMode, Stack, createTheme } from "@mui/material";
+import { Button, Stack } from "@mui/material";
 import Intro from "@features/home/Intro";
 import { toast } from "mui-sonner";
 import ToggleCustomTheme from "@features/theme/ThemeToggle";
-import getLPTheme from "@styles/dark";
-import { useState } from "react";
 
 export default function Home({
   toggleTheme,
@@ -15,96 +13,98 @@ export default function Home({
 }) {
   return (
     <Container maxWidth="lg">
-      <Intro />
-      <Stack alignItems="center" gap={5}>
-        <Button
-          variant="contained"
-          onClick={() =>
-            toast.info("You have a new message!", {
-              closeButton: true,
-              duration: Infinity,
-            })
-          }
-        >
-          Info
-        </Button>
-        <Button
-          variant="contained"
-          onClick={() =>
-            toast.success("Successfully updated!", {
-              duration: 20000,
-              action: {
-                label: "Undo",
-                buttonSx: {
-                  color: "inherit",
-                  p: 0,
-                  height: 30,
-                  borderRadius: 0,
+      <Stack width={1} alignItems="center" gap={1}>
+        <Intro />
+        <Stack alignItems="center" gap={5}>
+          <Button
+            variant="contained"
+            onClick={() =>
+              toast.info("You have a new message!", {
+                closeButton: true,
+                duration: Infinity,
+              })
+            }
+          >
+            Info
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() =>
+              toast.success("Successfully updated!", {
+                duration: 20000,
+                action: {
+                  label: "Undo",
+                  buttonSx: {
+                    color: "inherit",
+                    p: 0,
+                    height: 30,
+                    borderRadius: 0,
+                  },
+                  onClick: () => {
+                    console.log("Info");
+                  },
                 },
-                onClick: () => {
-                  console.log("Info");
+              })
+            }
+          >
+            Success
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() =>
+              toast.error("Sorry something went wrong!", {
+                variant: "filled",
+              })
+            }
+          >
+            Error
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => toast.warning("You have unsaved changes!")}
+          >
+            Warning
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => {
+              toast("Save Successful", {
+                severity: "success",
+                description: "Your item has been saved.",
+                action: {
+                  label: "Undo",
+                  onClick: () => {
+                    console.log("Undo");
+                  },
                 },
-              },
-            })
-          }
-        >
-          Success
-        </Button>
-        <Button
-          variant="contained"
-          onClick={() =>
-            toast.error("Sorry something went wrong!", {
-              variant: "filled",
-            })
-          }
-        >
-          Error
-        </Button>
-        <Button
-          variant="contained"
-          onClick={() => toast.warning("You have unsaved changes!")}
-        >
-          Warning
-        </Button>
-        <Button
-          variant="contained"
-          onClick={() => {
-            toast("Save Successful", {
-              severity: "success",
-              description: "Your item has been saved.",
-              action: {
-                label: "Undo",
-                onClick: () => {
-                  console.log("Undo");
-                },
-              },
-            });
-          }}
-        >
-          Custom
-        </Button>
-        <Button
-          variant="contained"
-          onClick={() => {
-            toast.loading("Loading...", {
-              id: "loader",
-            });
-            setTimeout(() => {
-              toast.dismiss("loader");
-            }, 1000);
-          }}
-        >
-          Loading
-        </Button>
+              });
+            }}
+          >
+            Custom
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => {
+              toast.loading("Loading...", {
+                id: "loader",
+              });
+              setTimeout(() => {
+                toast.dismiss("loader");
+              }, 1000);
+            }}
+          >
+            Loading
+          </Button>
 
-        <Button variant="contained" onClick={() => toast.dismiss()}>
-          Dismiss all
-        </Button>
+          <Button variant="contained" onClick={() => toast.dismiss()}>
+            Dismiss all
+          </Button>
+        </Stack>
+        <ToggleCustomTheme
+          showCustomTheme={showCustomTheme}
+          toggleCustomTheme={toggleTheme}
+        />
       </Stack>
-      <ToggleCustomTheme
-        showCustomTheme={showCustomTheme}
-        toggleCustomTheme={toggleTheme}
-      />
     </Container>
   );
 }
