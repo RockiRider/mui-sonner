1c586b839273d82dc20e508c806b24b07ff8a47a
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-02-24 20:09:46 +0000

    update theme

--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -1,7 +1,28 @@
 export default {
-  logo: <span>MUI Sonner</span>,
+  logo: <span style={{ fontWeight: 600 }}>MUI Sonner</span>,
   project: {
-    link: "https://github.com/shuding/nextra",
+    link: "https://github.com/RockiRider/mui-sonner",
+  },
+  docsRepositoryBase:
+    "https://github.com/RockiRider/mui-sonner/tree/main/apps/docs",
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s | MUI Sonner",
+    };
+  },
+  feedback: {
+    content: null,
+  },
+  footer: {
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{" "}
+        <a href="https://mui-sonner.tsotne.co.uk" target="_blank">
+          MUI Sonner
+        </a>
+        .
+      </span>
+    ),
   },
   // ... other theme options
 };
