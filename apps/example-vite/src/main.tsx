c53dd1ccc101fc7cc7884686aeaa11bb1a384abc
Tsotne Gvadzabia <32150283+RockiRider@users.noreply.github.com>
2024-02-26 21:31:43 +0000

    Feat/Action + Close Button (#16)

--- apps/example-vite/src/main.tsx
+++ apps/example-vite/src/main.tsx
@@ -3,10 +3,23 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Toaster } from "mui-sonner";
+import { CircularProgress, Icon } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Toaster />
+    <Toaster
+      toastOptions={{
+        closeIcon: (
+          <Icon sx={{ width: 24, height: 24 }}>
+            <CloseIcon />
+          </Icon>
+        ),
+        loading: {
+          icon: <CircularProgress size={20} color="secondary" />,
+        },
+      }}
+    />
     <App />
   </React.StrictMode>
 );
